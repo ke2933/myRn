e382d39c91679515230266f99a374e10c061e3cf
ke2933 <534144810@qq.com>
2018-11-08 18:04:40 +0800

    添加第三方插件

--- android/app/src/main/java/com/myrn/MainApplication.java
+++ android/app/src/main/java/com/myrn/MainApplication.java
@@ -3,6 +3,12 @@ package com.myrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +28,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new SvgPackage(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new PickerPackage(),
+            new RNFetchBlobPackage(),
+            new RNDeviceInfo()
       );
     }
 
